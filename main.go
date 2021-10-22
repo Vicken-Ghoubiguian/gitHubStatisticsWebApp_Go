ef18e486a17396640538e1619345e27c7dc9bb04
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2021-10-22 19:38:05 +0200

    KIOOO 4

--- main.go
+++ main.go
@@ -16,6 +16,10 @@ func main() {
 	http.HandleFunc("/user", serverFunctions.GitHubUserFunc)
 	http.HandleFunc("/org", serverFunctions.GitHubOrganizationFunc)
 	http.HandleFunc("/repos", serverFunctions.GitHubRepositoryFunc)
+	http.HandleFunc("/commit", serverFunctions.GitHubCommitfunc)
+	http.HandleFunc("/lang", serverFunctions.GitHubLanguagefunc)
+	http.HandleFunc("/issue", serverFunctions.GitHubIssuefunc)
+	http.HandleFunc("/license", serverFunctions.GitHubLicensefunc)
 
 	//
 	http.ListenAndServe(":80", nil)
